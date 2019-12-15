cdd778a5f5515c1121d1f13988dd4ec0aac956f4
WouterKromkamp <423394@student.saxion.nl>
2019-12-15 16:10:39 +0100

    renamed to script

--- setupcomplete.cmd
+++ setupcomplete.cmd
@@ -1,2 +1,2 @@
 SET LOCATION=%cd%
-PowerShell -NoProfile -ExecutionPolicy Unrestricted -Command "& {Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Unrestricted -File ""%LOCATION%\Win10AfterSetup.ps1""' -Verb RunAs}";
+PowerShell -NoProfile -ExecutionPolicy Unrestricted -Command "& {Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Unrestricted -File ""%LOCATION%\script.ps1""' -Verb RunAs}";
